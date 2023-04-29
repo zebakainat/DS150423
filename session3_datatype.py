22c6a2c2eb04808c4f4f629c088d6b0048e4d9c0
zebakainat <89031431+zebakainat@users.noreply.github.com>
2023-04-29 19:51:42 +0530

    Add files via upload

---
+++ session3_datatype.py
@@ -0,0 +1,46 @@
+# 1. INTEGER
+#num1 = 100
+#print(type(num1))
+
+# 2. FLOAT
+#num2 = 15.25
+#print(type(num2))
+
+#--Immutable datatype examples
+#num1 = 100
+#print(id(num1))
+#num1 = 105
+#print(id(num1))
+
+# 3. STRING
+#s = "'Python' Sample String"
+#print(s,type(s),id(s))
+#s = "new value"
+#print(s,type(s),id(s))
+
+# 4. LISTS
+#l = [10,20,30,40,50]
+#print(l)
+
+#l = [10,20,30,40,50,"Python","Django"]
+#print(id(l))
+#l.append(60)
+#print(l,id(l))
+
+# 5. TUPLE
+#  t = (10,20,30)
+# print(t)
+
+# 6. DICTIONARY
+#d = {"name":"ABC","email":"abc@gmail.com"}
+#print(d)
+
+# 7. SETS
+#s = {10,20,30,40}
+#print(s)
+
+# 8. BOOLEAN : True,False
+
+# 9. COMPLEX : (4 + 3j)
+
+help(str)
